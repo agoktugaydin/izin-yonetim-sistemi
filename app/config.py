5e28ca955ebe7f149403d93e97feb78d78a65ffd
agoktugaydin <goktugaydin36@gmail.com>
2023-07-24 01:13:08 +0300

    update config

--- app/config.py
+++ app/config.py
@@ -10,13 +10,8 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    DATABASE_PORT: int = os.getenv("DATABASE_PORT")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
-    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
-    POSTGRES_HOSTNAME: str = os.getenv("POSTGRES_HOSTNAME")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
     @property
     def database_url(self) -> str:
-        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.DATABASE_PORT}/{self.POSTGRES_DB}"
+        return self.DATABASE_URL
